53353b3c53432231909125c2998b3d1a564abc23
baobann01 <hgbaovip115@gmail.com>
2025-06-05 13:10:04 +0700

    Your commit message, e.g., 'Update App.jsx'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LandingPage from './pages/LandingPage'
-// import AdoptPage from './pages/AdoptPage'
-// import PostPage from './pages/PostPage'
-// import BlogPage from './pages/BlogPage'
-// import LoginPage from './pages/LoginPage'
-// import RegisterPage from './pages/RegisterPage'
-// import AboutPage from './pages/AboutPage'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+// import AdoptPage from './pages/AdoptPage';
+// import PostPage from './pages/PostPage';
+// import BlogPage from './pages/BlogPage';
+// import LoginPage from './pages/LoginPage';
+// import RegisterPage from './pages/RegisterPage';
+import AboutPage from './pages/AboutPage';
+import DonatePage from './pages/DonatePage'; // Import DonatePage
+import Navbar from './components/Navbar'; // Uncomment this line to import Navbar
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -19,11 +20,11 @@ export default function App() {
           <Route path="/posts" element={<PostPage />} />
           <Route path="/blog" element={<BlogPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/about" element={<AboutPage />} /> */}
+          <Route path="/register" element={<RegisterPage />} /> */}
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/donate" element={<DonatePage />} /> {/* Thêm route cho DonatePage */}
         </Routes>
       </div>
-      <Footer />
     </BrowserRouter>
-  )
+  );
 }
