43190777fb107e2a9f7cd7ecf759595ab39f093d
Nguyen Duc Nguyen <nguyenndse182830@fpt.edu.vn>
2025-06-08 10:24:24 +0700

    commit

--- client/src/pages/ForgotPasswordPage.jsx
+++ client/src/pages/ForgotPasswordPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import bgImage from '../assets/catdog.jpg'; // Using the same background
 import logoImg from '../assets/paw-logo.png'; // Using the same logo
-import pawLetter from '../assets/PawLetter.png';
+import pawLetter from '../assets/pawLetter.png';
 import '../styles/ForgotPasswordPage.css'; // Corresponding CSS file
 
 const ForgotPasswordPage = () => {
