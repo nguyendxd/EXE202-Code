f90baf274a9091e810897fc471eb556475ad2a5a
Nguyen Duc Nguyen <nguyenndse182830@fpt.edu.vn>
2025-06-08 19:49:20 +0700

    delete app.use(cors)

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,7 +16,6 @@ import rescueStationRoutes from './routes/rescueStationRoutes';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
 
